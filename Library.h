7a70e0bb4da82ece7efb60e388ced8c2d09d91e3
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-05-27 19:10:42 +0300

    Update Library.h

--- Library.h
+++ Library.h
@@ -6,15 +6,36 @@ struct node
 	struct note *right;
 	struct node *left;
 };
-void HeapSort(struct node* root);
 
-void ConstructHeap(int n, struct node *root);
+typedef struct Nod 
+{
+	int data;                                   //ce va retine fiecare nod
+	struct Nod * next;                          //pointeri catre elementul urmator
+}Nod;
 
-void AddtoHeap(struct node *root, int value);
+void print_list(Nod *head);
 
-void MergeSort(float vector[100000], int beg, int end,float B[100000]);
+void push_first(Nod *head, int val);
 
-void MergeParts(float A[100000], int beg, int mid, int end, float B[100000]);
+void push_last(Nod *head, int val);
+
+void push_poz(Nod *head, int poz, int val);
+
+int pop_first(Nod * head);
+
+int pop_last(Nod * head);
+
+int pop_poz(Nod *head, int poz);
+
+void heapsort(struct node* root);
+
+void constructheap(int n, struct node *root);
+
+void addtoheap(struct node *root, int value);
+
+void mergesort(float vector[100000], int beg, int end,float B[100000]);
+
+void mergeparts(float A[100000], int beg, int mid, int end, float B[100000]);
 
 void readarray(int n, float vector[100000]);
 
