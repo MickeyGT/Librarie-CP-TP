eb9f2a1be069820605e128cc55be9932187618d1
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-05-29 19:02:58 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -2,7 +2,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 float v[100000], v1[100000];
-int n, el;
+int n, el,i,nr, vizitat[100],matrice[100][100],j;
+
+void dfs(int root, int number)
+{
+	int k;
+	vizitat[root] = 1;
+	for (k = 1;k <= number;k++)
+		if (matrice[root][k] && !vizitat[k])
+		{
+			printf("\n%d->%d", root, k);
+			dfs(k, number);
+		}
+}
+
+void bfs( int number, int v)
+{
+    int front, rear,k;
+	int que[20];
+	front = rear = -1;
+	for (k = 1;k <= number;k++)
+		vizitat[k] = 0;
+
+	printf("%d ", v);
+	vizitat[v] = 1;
+	rear++;
+	front++;
+	que[rear] = v;
+
+	while (front <= rear)
+	{
+		v = que[front]; // delete from queue 
+			front++;
+			for (k = 1;k <= number;k++)
+		{
+			// Check for adjacent unvisited nodes 
+				if (matrice[v][k] == 1 && vizitat[k] == 0)
+				{
+					printf("%d ", k);
+					vizitat[k] = 1;
+					rear++;
+					que[rear] = k;
+				}
+		}
+	}
+}
+
 int main()
 {
 	/*
@@ -19,7 +64,9 @@ int main()
 	printf("The sorted elements from Heap:\n");
 	HeapSort(root);
 	*/
-	///////////////////////////////////////////////
+
+	//////////////////////////////////////////////////
+
 	/*
 	printf("N=");
 	scanf("%i", &n);
@@ -27,8 +74,10 @@ int main()
 	MergeSort(v, 1, n+1,v1);
 	printarray(n, v1);
 	*/
-	int aux;
 
+	//////////////////////////////////////////////////
+
+	/*
 	Nod * head = NULL; // creez nodul de start
 	head = (Nod*)malloc(sizeof(Nod)); // aloc memorie pentru nodul de start
 	head->next = NULL;
@@ -50,6 +99,27 @@ int main()
 	print_list(head);
 	pop_poz(head, 3);
 	print_list(head);
+	*/
+	/*scanf("%d", &nr);
+	for (i = 1;i <= nr;i++)
+		for (j = 1;j <= nr;j++)
+			scanf("%d", &matrice[i][j]);
+	*/
+
+	//////////////////////////////////////////////////
+
+
+	FILE *f;
+	f= fopen("file.in","r");
+	fscanf(f,"%d", &n);
+	for (i = 1;i<=n;i++)
+		vizitat[i] = 0;
+	for (i = 1;i <= n;i++)
+		for (j = 1;j <= n;j++)
+			fscanf(f,"%d ", &matrice[i][j]);
+	dfs(1, n);
+	printf("\n");
+	bfs(n, 1);
 	system("pause");
 	return 0;
 }
